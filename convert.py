64ae3964367639944876cff9a1b9c35190eb2909
Tom Anthony <git@tomanthony.co.uk>
2018-06-12 15:23:00 +0100

    Add readme. Clean up dev scrun a bit.

--- convert.py
+++ convert.py
@@ -17,8 +17,6 @@ blank_slide_layout = prs.slide_layouts[6]
 
 # Create working folder
 base_name = pdf_file.split(".pdf")[0]
-# if not os.path.exists(base_name):
-# 	os.makedirs(base_name)
 
 # Convert PDF to list of images
 print("Starting conversion...")
@@ -30,28 +28,12 @@ print()
 for i, slideimg in enumerate(slideimgs):
 	if i % 10 == 0:
 		print("Saving slide: " + str(i))
-	# filepath = os.path.join(base_name, 'temp_' + str(i) + '.jpg')
 
 	imagefile = BytesIO()
 	slideimg.save(imagefile, format='tiff')
 	imagedata = imagefile.getvalue()
 	imagefile.seek(0)
-
-	# slideimg.save(filepath, 'JPEG')
-	# output = StringIO()
-	# slide.save(output, format="JPEG")
 	width, height = slideimg.size
-	# print(slideimg.size)
-	# contents = output.getvalue()
-	# output.close()
-
-	# (5000, 2813) # 500
-	# (3000, 1688) # 300
-
-	# Get image dimensions
-	# im = Image.open(filepath)
-	# width, height = im.size
-	# print(im.size)
 
 	# Set slide dimensions
 	prs.slide_height = height * 9525
@@ -59,7 +41,6 @@ for i, slideimg in enumerate(slideimgs):
 
 	# Add slide
 	slide = prs.slides.add_slide(blank_slide_layout)
-	# pic = slide.shapes.add_picture(filepath, 0, 0, width=width * 9525, height=height * 9525)
 	pic = slide.shapes.add_picture(imagefile, 0, 0, width=width * 9525, height=height * 9525)
 
 # Save Powerpoint
